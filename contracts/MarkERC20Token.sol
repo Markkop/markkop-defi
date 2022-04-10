56543686d42cee773ec13df3210bc184e825d214
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-04-10 17:31:59 -0300

    feat: erc20 token

---
+++ contracts/MarkERC20Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MarkERC20Token is ERC20, Ownable {
+    constructor() ERC20("Mark", "MARK") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
